005a0144788f5930da43d340b4cad2747eda76e1
Lucus Lally <lucuslally1@gmail.com>
2018-04-11 22:30:06 -0700

    Added toHex & Constants examples from page #32 & #33

--- MavenModule/src/test/java/IntegerTestExamples.java
+++ MavenModule/src/test/java/IntegerTestExamples.java
@@ -1,4 +1,4 @@
-// page 32
+// page 32 & 33
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,33 +6,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IntegerTestExamples {
 
     @Test
-    void integerExploration(){
+    void integerExploration() {
         Integer four = 4;
-        assertEquals(4, four.intValue(),"intValue returns int 4");
+        assertEquals(4, four.intValue(), "intValue returns int 4");
 
         Integer five = 5;
-        assertEquals(5, five.intValue(),"intValue returns int 5" );
+        assertEquals(5, five.intValue(), "intValue returns int 5");
 
     }
+}
+
+class HexStringExamples {
 
     @Test
-    static void toHexStringExercises(){
+    void toHexStringExercises() {
         // 11 to b
-        Integer eleven = 11;
-        assertEquals('b' , eleven.toHexString(11), "Hex String 11 to b");
+        assertEquals("b", Integer.toHexString(11), "Hex String 11 to b");
 
         // 10 to a
-        Integer ten = 10;
-        assertEquals('a' , eleven.toHexString(10), "Hex String 10 to a");
+        assertEquals("a", Integer.toHexString(10), "Hex String 10 to a");
 
         // 3 to 3
-        Integer three = 3;
-        assertEquals(3 , eleven.toHexString(3), "Hex String 3 to 3");
+        assertEquals("3", Integer.toHexString(3), "Hex String 3 to 3");
 
         // 21 to 15
-        Integer twentyone = 21;
-        assertEquals(15 , eleven.toHexString(21), "Hex String 21 to 15");
+        assertEquals("15", Integer.toHexString(21), "Hex String 21 to 15");
     }
+}
 
+class ConstantsExamples{
 
+    @Test
+    void publicConstantsExercises(){
+        Integer minimum = -2147483648;
+        assertEquals(-2147483648, Integer.MIN_VALUE, "Minimum value of int is -2147483648");
+
+        Integer maximum = 2147483647;
+        assertEquals(2147483647 , Integer.MAX_VALUE, "Maximum value of int is 2147483648");
+    }
 }
